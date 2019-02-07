b19e4ea7c01653497c94d63a5a496e12a41f02d0
Eduardo Satyra <edufelipefreitas2@gmail.com>
2019-02-06 23:25:42 -0200

    configurando travis-CI

---
+++ src/main/java/br/com/eduardosatyra/api/TesteApiRestfulApplication.java
@@ -0,0 +1,14 @@
+package br.com.eduardosatyra.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteApiRestfulApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/eduardosatyra/api/TesteApiRestfulApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.eduardosatyra.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TesteApiRestfulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
